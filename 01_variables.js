f48cba5cc42f1ea2661c0c92c30412de6c87ed9e
Jiya Hingrajiya <jiya@Jiyas-MacBook-Air.local>
2025-10-25 00:18:43 +0530

    Initial commit

---
+++ 01_variables.js
@@ -0,0 +1,21 @@
+const accntId = 345;
+let accEmail = "abc@gmail.com";
+var accPass = "1244";
+accCity = "Delhi";
+let name 
+
+// accntId=456; // not allowed
+
+accEmail = "jiya@gmail.com";
+accPass = "98765";
+accCity = "Noida";
+
+
+console.log(accntId);
+
+console.table([accEmail, accPass, accCity,name]);
+
+//prefered way to declare a variable - let 
+
+//not prefered way to declare a variable - var as it creates a global variable which can be accessed outside the block scope
+
---
+++ 02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert (2+2) // using node.js , not browser
+
+console.log("hello bitches");
+
+console.log(19-9);
+
+//datatypes in js
+//primitive datatypes: string, number, boolean, undefined, null, symbol
+//reference datatypes: object
+
+
+let name = "jiya"; // string
+let age = 19; // number
+let isApproved = false; // boolean
+let firstName = undefined; // undefined
+let selectedColor = null; // null
+//symbol - used for unique identifiers
+
+console.log(typeof "joy");//string
+console.log(typeof null);//object 
+console.log(typeof undefined);//undefined
+
